9a0070b8ea7340d170c70fbca7c4791345bdfafc
Talescruzs <talescruzdasilva@gmail.com>
2025-11-20 18:19:57 -0300

    add t3 do benhur, vou tentar por aqui

---
+++ t3/Codigo/controle.d
@@ -0,0 +1,2 @@
+controle.o controle.d : controle.c controle.h cpu.h es.h err.h dispositivos.h \
+ terminal.h irq.h mmu.h tabpag.h memoria.h console.h relogio.h
---
+++ t3/Codigo/main.d
@@ -0,0 +1,2 @@
+main.o main.d : main.c controle.h cpu.h es.h err.h dispositivos.h terminal.h \
+ irq.h mmu.h tabpag.h memoria.h console.h relogio.h programa.h so.h
